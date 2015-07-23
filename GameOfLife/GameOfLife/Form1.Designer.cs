6232d040ec542a79cb7f6f296b86462c37346d27
Huy Quan <anhhuydng@gmail.com>
2015-07-23 23:27:05 +0700

    Add picture box and some feature

--- GameOfLife/GameOfLife/Form1.Designer.cs
+++ GameOfLife/GameOfLife/Form1.Designer.cs
@@ -29,33 +29,107 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblGridSize = new System.Windows.Forms.Label();
+            this.numGridSize = new System.Windows.Forms.NumericUpDown();
+            this.numCellSize = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numGridSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCellSize)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(127, 12);
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(239, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(541, 370);
+            this.pictureBox1.Size = new System.Drawing.Size(541, 415);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Grid size:";
+            // 
+            // lblGridSize
+            // 
+            this.lblGridSize.AutoSize = true;
+            this.lblGridSize.Location = new System.Drawing.Point(174, 15);
+            this.lblGridSize.Name = "lblGridSize";
+            this.lblGridSize.Size = new System.Drawing.Size(42, 13);
+            this.lblGridSize.TabIndex = 4;
+            this.lblGridSize.Text = "10 x 10";
+            // 
+            // numGridSize
+            // 
+            this.numGridSize.Location = new System.Drawing.Point(68, 12);
+            this.numGridSize.Name = "numGridSize";
+            this.numGridSize.Size = new System.Drawing.Size(100, 20);
+            this.numGridSize.TabIndex = 5;
+            this.numGridSize.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numGridSize.ValueChanged += new System.EventHandler(this.numGridSize_ValueChanged);
+            // 
+            // numCellSize
+            // 
+            this.numCellSize.Location = new System.Drawing.Point(68, 38);
+            this.numCellSize.Name = "numCellSize";
+            this.numCellSize.Size = new System.Drawing.Size(100, 20);
+            this.numCellSize.TabIndex = 7;
+            this.numCellSize.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Cell size:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(792, 439);
+            this.Controls.Add(this.numCellSize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numGridSize);
+            this.Controls.Add(this.lblGridSize);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numGridSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCellSize)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblGridSize;
+        private System.Windows.Forms.NumericUpDown numGridSize;
+        private System.Windows.Forms.NumericUpDown numCellSize;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- GameOfLife/GameOfLife/Form1.cs
+++ GameOfLife/GameOfLife/Form1.cs
@@ -12,9 +12,49 @@ namespace GameOfLife
 {
     public partial class Form1 : Form
     {
+        int numOfCells = 10;
+        int cellSize = 10;
+        Graphics paper;
+        
         public Form1()
         {
             InitializeComponent();
+            paper = pictureBox1.CreateGraphics();
+            numOfCells = (int)numGridSize.Value;
+            cellSize = (int)numCellSize.Value;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Pen p = new Pen(Color.Black);
+            Pen pf = new Pen(Color.Green);
+            //Brush b = graphics.co;
+
+            for (int y = 0; y <= numOfCells; ++y)
+            {
+                graphics.DrawLine(p, 0, y * cellSize, numOfCells * cellSize, y * cellSize);
+            }
+
+            for (int x = 0; x <= numOfCells; ++x)
+            {
+                graphics.DrawLine(p, x * cellSize, 0, x * cellSize, numOfCells * cellSize);
+            }
+
+            //for (int y = 0; y < numOfCells; ++y)
+            //{
+            //    graphics.FillRectangle(pf.Brush, 0, y * cellSize, cellSize, cellSize);
+            //}
+
+            //for (int x = 0; x < numOfCells; ++x)
+            //{
+            //    graphics.FillRectangle(pf.Brush, x * cellSize, 0, x * cellSize, numOfCells * cellSize);
+            //}
+        }
+
+        private void numGridSize_ValueChanged(object sender, EventArgs e)
+        {
+            lblGridSize.Text = numGridSize.Value + " x " + numGridSize.Value;
         }
     }
 }
